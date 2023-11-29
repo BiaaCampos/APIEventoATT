8432f894cd1d82cd579ad9f1b6453bd2136b8929
1thiagoCRUZ <thiagocruz58452@gmail.com>
2023-11-29 10:36:06 -0300

    API Atualizada

---
+++ APIapresenta-o-main/DDD.Application.Api/Controllers/CompradorController.cs
@@ -0,0 +1,101 @@
+﻿using DDD.Domain.GeralContext;
+using DDD.Infra.SQLServer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DDD.Application.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompradorController : ControllerBase
+    {
+        readonly ICompradorRepository _compradorRepository;
+
+        public CompradorController(ICompradorRepository compradorRepository)
+        {
+            _compradorRepository = compradorRepository;
+        }
+
+        // GET: api/<CompradorController>
+        [HttpGet]
+        public ActionResult<List<Comprador>> Get()
+        {
+            return Ok(_compradorRepository.GetComprador());
+        }
+
+        // GET api/<CompradorController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Comprador> GetById(int id)
+        {
+            return Ok(_compradorRepository.GetCompradorById(id));
+        }
+
+        // POST api/<CompradorController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Comprador> CreateComprador(Comprador comprador)
+        {
+            _compradorRepository.InsertComprador(comprador);
+            return CreatedAtAction(nameof(GetById), new { id = comprador.Id }, comprador);
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Comprador comprador)
+        {
+            try
+            {
+                if (comprador == null || id != comprador.Id)
+                    return BadRequest("Invalid request data. Please check the request body and parameters.");
+
+                var existingComprador = _compradorRepository.GetCompradorById(id);
+
+                if (existingComprador == null)
+                {
+                    // Log that the Comprador was not found
+                    Console.WriteLine($"Comprador with id {id} not found.");
+                    return NotFound("Comprador not found.");
+                }
+
+                _compradorRepository.UpdateComprador(comprador);
+
+                // Log the successful update
+                Console.WriteLine($"Comprador with id {id} updated successfully.");
+
+                return Ok("Comprador updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception for further investigation
+                Console.WriteLine($"Error in PUT request: {ex.Message}");
+
+                // Return a more informative error message in the response body
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing the request. Please try again.");
+            }
+        }
+
+
+        // DELETE api/<CompradorController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var comprador = _compradorRepository.GetCompradorById(id);
+
+                if (comprador == null)
+                    return NotFound();
+
+                _compradorRepository.DeleteComprador(comprador);
+                return Ok("Comprador Excluído com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
+
---
+++ APIapresenta-o-main/DDD.Infra.SQLServer/Repositories/CompradorRepositorySqlServer.cs
@@ -0,0 +1,107 @@
+﻿using DDD.Domain.GeralContext;
+using DDD.Infra.SQLServer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DDD.Infra.SQLServer.Repositories
+{
+    public class CompradorRepositorySqlServer : ICompradorRepository
+    {
+        private readonly SqlContext _context;
+
+        public CompradorRepositorySqlServer(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteComprador(Comprador comprador)
+        {
+            try
+            {
+                _context.Set<Comprador>().Remove(comprador);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Log or handle specific database-related exceptions
+                throw new Exception("Error deleting Comprador. See inner exception for details.", ex);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle general exceptions
+                throw new Exception("An error occurred while deleting Comprador.", ex);
+            }
+        }
+
+        public Comprador GetCompradorById(int id)
+        {
+            try
+            {
+                return _context.Compradores.Find(id);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle exceptions
+                throw new Exception("An error occurred while retrieving Comprador by ID.", ex);
+            }
+        }
+
+        public List<Comprador> GetComprador()
+        {
+            try
+            {
+                return _context.Compradores.ToList();
+            }
+            catch (Exception ex)
+            {
+                // Log or handle exceptions
+                throw new Exception("An error occurred while retrieving Compradores.", ex);
+            }
+        }
+
+        public void InsertComprador(Comprador comprador)
+        {
+            try
+            {
+                _context.Compradores.Add(comprador);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Log or handle specific database-related exceptions
+                throw new Exception("Error inserting Comprador. See inner exception for details.", ex);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle general exceptions
+                throw new Exception("An error occurred while inserting Comprador.", ex);
+            }
+        }
+
+        public void UpdateComprador(Comprador comprador)
+        {
+            try
+            {
+                _context.Entry(comprador).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                // Log or handle concurrency-related exceptions
+                throw new Exception("Error updating Comprador. Concurrency conflict. See inner exception for details.", ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                // Log or handle specific database-related exceptions
+                throw new Exception("Error updating Comprador. See inner exception for details.", ex);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle general exceptions
+                throw new Exception("An error occurred while updating Comprador.", ex);
+            }
+        }
+    }
+}
